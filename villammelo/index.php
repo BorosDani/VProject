3014f4886de24a80884ec821fa1698cd00b4eefe
Gernya <103121757+KleberGergely@users.noreply.github.com>
2025-09-13 16:53:14 +0200

    Update index.php
    Amennyit tudtam, megcsináltam. az oldalcím + kép kimaradt az időhiány miatt. Remélem értékelhető

--- villammelo/index.php
+++ villammelo/index.php
@@ -8,8 +8,35 @@
 </head>
 <body>
     <div class='menu'>
+        <a href="index.php">Kezdőlap</a>
+        <a href="munkak.php">Munkák</a>
+        <label class="dropdown">
+        <input type="checkbox">
+        <span>Kategóriák </span>
+        <ul class="content">
+            <li><a href="kulteri.php">Kültéri munkák</a></li>
+            <li><a href="belteri.php">Beltéri munkák</a></li>
+        </ul>
+    </label>
         <a href="login.php">Bejelentkezés</a>
         <a href="register.php">Regisztráció</a>
     </div>
+    <footer>
+  <div class="footer-container">
+    <div>
+      <h4>Kapcsolat</h4>
+      <p>Email: <a href ="info@villammelo.hu">info@villammelo.hu</a></p>
+      <p>Telefon: insert telefonszám</p>
+    </div>
+
+    <div>
+      <h4>Kövess minket</h4>
+      <p><a href = "Facebook/villammelo">Facebook</a> | <a href = "Instagram/villammelo">Instagram</a></p>
+    </div>
+    <div>
+        <p>Minden jog fenntartva a Villámmeló programozóinak és vezetőinek. </p>
+</div>
+  </div>
+</footer>
 </body>
 </html>
