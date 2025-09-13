4a7344c8406aff373e5c7a835df33bf43614d0da
Dani <103130240+BorosDani@users.noreply.github.com>
2025-09-13 14:20:52 +0200

    VillamMelo

---
+++ villammelo/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Villám meló - főoldal</title>
+</head>
+<body>
+    <div class='menu'>
+        <a href="login.php">Bejelentkezés</a>
+        <a href="register.php">Regisztráció</a>
+    </div>
+</body>
+</html>
